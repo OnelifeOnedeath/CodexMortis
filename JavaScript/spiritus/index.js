707bd96e8f00d7cc201efd675c92a75bfe5e3da2
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 00:15:28 +0300

    Create index.js

---
+++ JavaScript/spiritus/index.js
@@ -0,0 +1,6 @@
+import { SpiritusEngine } from './src/core/SpiritusEngine.js';
+
+const spiritus = new SpiritusEngine();
+spiritus.ignite();
+
+console.log("Spiritus: Дух веба пробужден для Codex Mortis!");
