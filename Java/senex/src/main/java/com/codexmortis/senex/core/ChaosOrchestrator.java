9aea869a0e3abe5df9ad950f8f7a581b70b80224
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 00:37:33 +0300

    Update ChaosOrchestrator.java

--- Java/senex/src/main/java/com/codexmortis/senex/core/ChaosOrchestrator.java
+++ Java/senex/src/main/java/com/codexmortis/senex/core/ChaosOrchestrator.java
@@ -2,10 +2,10 @@ package com.codexmortis.senex.core;
 
 public class ChaosOrchestrator {
     public void initializeChaos() {
-        System.out.println("Senex: Оркестрирую многоязычный хаос!");
+        System.out.println("Senex оркестрирует многоязычный хаос!");
     }
     
-    public boolean isEnterpriseReady() {
-        return true; // Java всегда готова к Enterprise!
+    public String getLatinMotto() {
+        return "Java ex Senex - Ordo in Enterprise";
     }
 }
