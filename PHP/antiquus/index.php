b69af81de2c0be25b553465cd91ab4208146c7e3
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 00:23:57 +0300

    Update index.php

--- PHP/antiquus/index.php
+++ PHP/antiquus/index.php
@@ -1,1 +1,8 @@
+<?php
+require_once 'core/AncientWisdom.php';
 
+echo "ANTIQUUS: Древняя мудрость PHP пробуждена!\n";
+
+$wisdom = new AncientWisdom();
+echo $wisdom->awaken() . "\n";
+echo $wisdom->proclaimLatinMotto() . "\n";
