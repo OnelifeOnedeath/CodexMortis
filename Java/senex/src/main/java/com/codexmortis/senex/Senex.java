7fa63c88c3bc0b2f916e0b91c580f4ef02dccc23
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 00:37:13 +0300

    Update Senex.java

--- Java/senex/src/main/java/com/codexmortis/senex/Senex.java
+++ Java/senex/src/main/java/com/codexmortis/senex/Senex.java
@@ -4,7 +4,8 @@ import com.codexmortis.senex.core.ChaosOrchestrator;
 
 public class Senex {
     public static void main(String[] args) {
-        System.out.println("Senex: Enterprise мудрость пробуждена!");
+        System.out.println("Senex: Enterprise мудрость для Codex Mortis!");
+        System.out.println("Motto: Senex ex Java - Sapientia Enterprise");
         
         ChaosOrchestrator orchestrator = new ChaosOrchestrator();
         orchestrator.initializeChaos();
