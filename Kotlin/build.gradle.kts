330d7662d48b902d49a301b8d8bdca8198dc786e
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 02:33:32 +0300

    Update build.gradle.kts

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,1 +1,19 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
 
+group = "com.codexmortis"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("com.codexmortis.novus.NovusKt")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
