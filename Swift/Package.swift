3329e075347ea1866c0bd2ba0047c748db4fc865
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 02:32:19 +0300

    Update Package.swift

--- Swift/Package.swift
+++ Swift/Package.swift
@@ -1,1 +1,12 @@
+// swift-tools-version:5.7
+import PackageDescription
 
+let package = Package(
+    name: "Pulcher",
+    products: [
+        .executable(name: "Pulcher", targets: ["Pulcher"])
+    ],
+    targets: [
+        .executableTarget(name: "Pulcher")
+    ]
+)
