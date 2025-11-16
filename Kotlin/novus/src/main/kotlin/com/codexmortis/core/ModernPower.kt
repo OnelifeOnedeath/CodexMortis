f3e0c1c0ef82f85c6b4eb2843d43616ca2dcca49
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 02:33:17 +0300

    Update ModernPower.kt

--- Kotlin/novus/src/main/kotlin/com/codexmortis/core/ModernPower.kt
+++ Kotlin/novus/src/main/kotlin/com/codexmortis/core/ModernPower.kt
@@ -1,1 +1,15 @@
+package com.codexmortis.novus
 
+class ModernPower {
+    fun activate() {
+        println("Novus: Современная Android/JVM мощь активирована!")
+    }
+    
+    fun getStatus(): String {
+        return "Novus: Kotlin система готова к работе!"
+    }
+    
+    fun getLatinMotto(): String {
+        return "Kotlin ex Novus - Vis Moderna"
+    }
+}
