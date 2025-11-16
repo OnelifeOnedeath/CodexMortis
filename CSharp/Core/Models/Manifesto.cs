0b85224ecd7b779e655571a763c71ee5016b0576
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 00:35:17 +0300

    Update Manifesto.cs

--- CSharp/Core/Models/Manifesto.cs
+++ CSharp/Core/Models/Manifesto.cs
@@ -1,1 +1,12 @@
-
+namespace CodexMortis.Core.Models
+{
+    public class Manifesto
+    {
+        public string Title { get; } = "Codex Mortis";
+        public string LatinMotto { get; } = "Unus Vita, Unus Mors, Decem Linguae";
+        public string[] Languages { get; } = {
+            "C#", "Python", "Go", "Rust", "Java", 
+            "JavaScript", "PHP", "Swift", "Kotlin", "Bash"
+        };
+    }
+}
