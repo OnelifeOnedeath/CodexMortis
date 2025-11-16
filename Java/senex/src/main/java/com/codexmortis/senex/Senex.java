4cc15323c3cc0f4d3a4df35200cfa956510f4ddd
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 00:13:10 +0300

    Update Senex.java

--- Java/senex/src/main/java/com/codexmortis/senex/Senex.java
+++ Java/senex/src/main/java/com/codexmortis/senex/Senex.java
@@ -1,1 +1,12 @@
+package com.codexmortis.senex;
 
+import com.codexmortis.senex.core.ChaosOrchestrator;
+
+public class Senex {
+    public static void main(String[] args) {
+        System.out.println("Senex: Enterprise мудрость пробуждена!");
+        
+        ChaosOrchestrator orchestrator = new ChaosOrchestrator();
+        orchestrator.initializeChaos();
+    }
+}
