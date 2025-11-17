ec4ebf9eb7381b7d3c472c5c5b0979d6b793228a
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-17 03:37:31 +0300

    Update index.js

--- JavaScript/spiritus/index.js
+++ JavaScript/spiritus/index.js
@@ -1,8 +1,3 @@
 console.log("Spiritus: JavaScript дух для Codex Mortis!");
 console.log("Motto: Spiritus ex JavaScript - Anima Webi");
-
-import { SpiritusEngine } from './src/core/SpiritusEngine.js';
-
-const spiritus = new SpiritusEngine();
-spiritus.ignite();
-console.log(spiritus.getStatus());
+console.log("✅ JavaScript готов к работе!");
